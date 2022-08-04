fb515bfe08b2e3fe3e9aeff334da5a194ba87bcf
czkirilo <caiquez.kirilo@hotmail.com>
2022-08-04 19:54:41 -0300

    ER2

---
+++ Sistema/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema
+{
+    public class Pessoa
+    {
+       public string Nome { get; set; }
+       public string Endereco { get; set; }
+       public bool enderecoComercial { get; set; }
+    }
+}
